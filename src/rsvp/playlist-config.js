68ba96118bccb7a8944eceb674d5e6ce2aadd303
Mose James <262014973+mosejames@users.noreply.github.com>
2026-09-20 07:59:36 -0400

    Let karaoke families listen directly to their shared Spotify playlist

--- src/rsvp/playlist-config.js
+++ src/rsvp/playlist-config.js
@@ -1,3 +1,3 @@
 // Set only to the organizer's actual playlist URL, never a Spotify search.
-export const SPOTIFY_PLAYLIST_URL = null;
+export const SPOTIFY_PLAYLIST_URL = 'https://open.spotify.com/playlist/6DlANRc4uEDTSFAfKfBFoz';
 export const PLAYLIST_PAGE = '/karaoke/playlist';
